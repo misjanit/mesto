bea385509e6e66d1f47f56869bb623ee0f2f73e6
Андрей Лиханов <mogueta63@gmail.com>
2022-02-06 16:54:25 +0500

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,54 @@
+/* Общие классы */
+const elements = document.querySelector('.elements'); // переменная для контейнера где хранятся все карточки
 const popup = document.querySelector('.popup');
-const popupEditProfile = document.getElementById('popup-edit-profile');
-const buttonClose = document.getElementById('close-edit-popup');
-const buttonCloseCardPopup = document.getElementById('close-card-popup');
-const buttonCloseWindowPopup = document.getElementById('close-window-popup');
-const popupButton = document.querySelector('.profile__edit-button');
-const popupForm = document.getElementById('popup-profile-form');
-const popupModal = document.getElementById('popup-modal');
-
+const popupClose = document.querySelectorAll('.popup__close-button'); // Кнопка закрытия любого Popup
+/* Блок Profile */
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
+/* Popup редактирования профия */
+const popupEditProfile = document.getElementById('popup-edit-profile');
+const openEditPopup = document.querySelector('.profile__edit-button'); // Открытие popup изменения профиля
+const buttonClose = document.getElementById('close-edit-popup');
 const popupName = document.getElementById('username');
 const popupDescription = document.getElementById('description');
+const popupForm = document.getElementById('popup-profile-form');
+/* Popup добавления карточки */
+const openPopupNewCard = document.querySelector('.profile__add-button'); // Открытие popup новой карточки места
+const popupNewCard = document.getElementById('popup-new-card'); // Popup новой карточки места
+const buttonCloseCardPopup = document.getElementById('close-card-popup'); 
+const popupCardForm = document.getElementById('popup-card-form'); // переменная формы с инпутами и кнопкой сохранить у PopUp-Card
+const popupCardTitle = document.getElementById('title'); // переменная инпута текста PopUp-Card
+const popupCardImage = document.getElementById('image'); // переменная инпута картинки PopUp-Card
+const template = document.getElementById('template'); // переменная заготовки карточки
+/* Popup модального окна картинки */
+const popupModal = document.getElementById('popup-modal');
+const buttonCloseWindowPopup = document.getElementById('close-window-popup');
 
-popupButton.addEventListener('click', openPopup); // Открытие PopUp
 
-function openPopup() {
-    popupEditProfile.classList.add('popup_opened');
+/*popupButton.addEventListener('click', popupEditValue); // Записывает значения из профиля в PopUp
+
+function popupEditValue() {
     popupName.value = profileName.textContent;
     popupDescription.value = profileDescription.textContent;
+} */
+
+function addListeners(el) {
+    el.querySelector('.profile__edit-button');
+    el.querySelector('.profile__add-button');
 }
 
-const addButton = document.querySelector('.profile__add-button');
-const popupNewCard = document.getElementById('popup-new-card');
 
-addButton.addEventListener('click', openPopupNewCard); // Открытие PopUp карточки
 
-function openPopupNewCard() {
-    popupNewCard.classList.add('popup_opened');
-}
 
-buttonClose.addEventListener('click', closePopup); // Закрытие PopUp профиля
-buttonCloseCardPopup.addEventListener('click', closePopup); // Закрытие PopUp карточки
-buttonCloseWindowPopup.addEventListener('click', closePopup); // Закрытие PopUp модального окна
 
-function closePopup() {
-    popupNewCard.classList.remove('popup_opened');
-    popupEditProfile.classList.remove('popup_opened');
-    popupModal.classList.remove('popup_opened');
-}
+
+
 
 popup.addEventListener('click', function(event) { // Закрытие любого PopUp при клике мимо него
     if(!event.defaultPrevented) {
       closePopup();
     }
-  });
+  }); 
 
 // Записать введенные значения из PopUp в профиль
 popupForm.addEventListener('submit', submitProfileButtonReaction);
@@ -54,12 +59,6 @@ function submitProfileButtonReaction(evt) {
     profileDescription.textContent = popupDescription.value;
     closePopup();
 }
-
-const popupCardForm = document.getElementById('popup-card-form'); // переменная формы с инпутами и кнопкой сохранить у PopUp-Card
-const popupCardTitle = document.getElementById('title'); // переменная инпута текста PopUp-Card
-const popupCardImage = document.getElementById('image'); // переменная инпута картинки PopUp-Card
-const elements = document.querySelector('.elements'); // переменная для контейнера где хранятся все карточки
-const template = document.getElementById('template'); // переменная заготовки карточки
 
 const initialCards = [ // Массив
     {
@@ -113,6 +112,7 @@ function addNewCard(card) {
 
         const popupImage = document.querySelector('.popup__image'); // Передаем нужную картинку в модальное окно
         popupImage.src = elementImage;
+        popupImage.alt = elementTitle;
 
         const popupDescription = document.querySelector('.popup__description');
         popupDescription.textContent = elementTitle;
